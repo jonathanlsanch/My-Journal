0c2058ddcadd1e56ddd0f541fc171afaecc68ecc
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-02-27 13:49:33 -0500

    Add AOS animation

---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+AOS.init({
+    duration: 2000
+});
+
+$(window).on('load', function () {
+    AOS.refresh();
+});
+
+
+
+
+//   $(function() {
+//     AOS.init({
+//         // duration: 1200,
+//       });
+//   });
